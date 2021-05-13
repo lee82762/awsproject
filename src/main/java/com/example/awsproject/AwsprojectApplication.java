9b226e472146f9a70542dea677785e19f47332ec
Leejaebum <cjdrn0208@naver.com>
2021-05-13 18:31:07 +0900

    awsproject

---
+++ src/main/java/com/example/awsproject/AwsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/awsproject/AwsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
